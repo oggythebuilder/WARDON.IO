9488d04dbc7ad8c7a652f881959b68696f790f6c
oggythebuilder <oggythebuilder01@gmail.com>
2026-02-24 22:19:31 +0530

    http-headers

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: false, 
+  // Deployment ke 404 errors ko fix karne ke liye headers settings
+  async headers() {
+    return [
+      {
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
